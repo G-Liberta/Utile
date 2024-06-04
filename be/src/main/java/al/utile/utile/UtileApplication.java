7eb258ec4ab10aa57af3a9b31f0f04c8627f205f
ndodaj <ndodaj@outlook.com>
2024-06-05 00:50:29 +0200

    initial commit

---
+++ be/src/main/java/al/utile/utile/UtileApplication.java
@@ -0,0 +1,13 @@
+package al.utile.utile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtileApplication.class, args);
+	}
+
+}
---
+++ be/src/main/java/al/utile/utile/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package al.utile.utile.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationEventPublisher publisher) throws Exception {
+        return http
+
+                // apply security
+
+                .build();
+    }
+
+}
